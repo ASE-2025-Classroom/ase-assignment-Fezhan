a83f46f1ea3fde74097bbc2da51481f833dfe1e3
Fezhan <f.lohani2662@student.leedsbeckett.ac.uk>
2025-09-24 21:12:21 +0100

    Created initial dotnet console

---
+++ BOOSEexamplePrograms/1BasicDrawing/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _1BasicDrawing;
+
+using BOOSE;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var version = AboutBOOSE.Version;
+        var info = AboutBOOSE.about();
+    }
+}
