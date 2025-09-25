7114c1d7a62d53930cdf7de59318b25f0ddde3c2
Fezhan <f.lohani2662@student.leedsbeckett.ac.uk>
2025-09-25 11:13:18 +0100

    Moved files to folder 6addYourProgramsHere

---
+++ BOOSEexamplePrograms/6addYourProgramsHere/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _1BasicDrawing;
+
+using BOOSE;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var version = AboutBOOSE.Version;
+        var info = AboutBOOSE.about();
+        Console.WriteLine("=== BOOSE Information ===");
+        Console.WriteLine($"Version: {version}");
+        Console.WriteLine("About: ");
+        Console.WriteLine(info);
+        Console.WriteLine("=========================");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+
+
+    }
+}
